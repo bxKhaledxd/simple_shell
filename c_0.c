b5622ec72d6507ab91b54593ffea058819738f04
bxKhaledxd <125661319+bxKhaledxd@users.noreply.github.com>
2023-05-18 00:00:20 +0300

    Add files via upload

---
+++ c_0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int k;
+
+	for (k = 0; k < 26; k++)
+	{
+		putchar(alp[k]);
+	}
+    
+	putchar('\n');
+	return (0);
+}
